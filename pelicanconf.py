991ba8592947dcf5da0dfb90429c56ccab7871e6
yxwangnju <yuxinwangcs@outlook.com>
2021-05-27 01:10:50 +0800

    .

--- pelicanconf.py
+++ pelicanconf.py
@@ -39,7 +39,8 @@ SOCIAL = (('Twitter', 'https://twitter.com'),)
 MENUITEMS_NAVBAR = (("About", "/pages/about.html"), 
                     ("How to set this page", "/pages/how-to-set-github-page.html"))
 
-MENUITEMS_NAVBAR_FEATURED = (("Tags", "/tags.html", "is-link"),)
+MENUITEMS_NAVBAR_FEATURED = (("Tags", "/tags.html", "is-link"),
+                             ("Archives", "/archives.html", "is-link"))
 
 DEFAULT_PAGINATION = False
 
