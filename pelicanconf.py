25d0a605d26938b462ac502f1dc547a3b602635b
yxwangnju <yuxinwangcs@outlook.com>
2021-05-27 01:28:56 +0800

    .

--- pelicanconf.py
+++ pelicanconf.py
@@ -40,7 +40,8 @@ MENUITEMS_NAVBAR = (("About", "/pages/about.html"),
                     ("How to set this page", "/pages/how-to-set-github-page.html"))
 
 MENUITEMS_NAVBAR_FEATURED = (("Tags", "/tags.html", "is-link"),
-                             ("Archives", "/archives.html", "is-link"))
+                             ("Archives", "/archives.html", "is-link"),
+                             ("Other Links", "/pages/other-links.html", "is-info"))
 
 DEFAULT_PAGINATION = False
 
