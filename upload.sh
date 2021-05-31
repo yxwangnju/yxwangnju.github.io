a34e78b5c32897749f52096a5d9ce8b27714b408
yxwangnju <yuxinwangcs@outlook.com>
2021-05-31 14:18:55 +0800

    .

--- upload.sh
+++ upload.sh
@@ -6,7 +6,7 @@ git add -u
 git commit -m "."
 git push origin notebook_source:notebook_source
 
-ghp-import -m "Generate Pelican site" --no-jekyll -b main output
+ghp-import -m "Generate Pelican site" --no-jekyll -b main output -c audreyw.top
 
 git push origin main:main
 
